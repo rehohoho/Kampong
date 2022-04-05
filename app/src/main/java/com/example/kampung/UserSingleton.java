c4b0d2da02c5567dd2964f64852ef08659da12c1
rehohoho <ernie.birdy.ho@gmail.com>
2022-04-05 14:28:41 +0800

    Update UserSingleton to implement ViewModel.

--- app/src/main/java/com/example/kampung/UserSingleton.java
+++ app/src/main/java/com/example/kampung/UserSingleton.java
@@ -1,4 +1,34 @@
 package com.example.kampung;
 
-public class UserSingleton {
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.kampung.models.User;
+
+
+/**
+ * ViewModel helps prepare data for UI, retained during configuration changes,
+ * available to next fragment / activity instance.
+ */
+public class UserSingleton extends ViewModel {
+
+    private MutableLiveData<User> userData;
+
+    private UserSingleton() {
+
+    }
+
+    private LiveData<User> getUser() {
+        if (userData == null) {
+            userData = new MutableLiveData<>();
+            loadUser();
+        }
+        return userData;
+    }
+
+    private void loadUser() {
+        // asynchronously load userData here
+    }
+
 }
