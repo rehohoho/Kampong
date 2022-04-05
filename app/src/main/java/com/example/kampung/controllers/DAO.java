88a0dd9222ba450d71eb80134ab73bfdec8f54f9
rehohoho <ernie.birdy.ho@gmail.com>
2022-04-06 00:10:45 +0800

    Rename DAORequest to DAO. Separate user and request ref.

---
+++ app/src/main/java/com/example/kampung/controllers/DAO.java
@@ -0,0 +1,46 @@
+package com.example.kampung.controllers;
+
+import com.example.kampung.models.Order;
+import com.example.kampung.models.Request;
+import com.example.kampung.models.SmolRequest;
+import com.example.kampung.models.User;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DAO {
+
+    private final String TAG = "DAO"
+    private final String DB_USER_ID = "senat";
+
+    private DatabaseReference requestDb;
+    private DatabaseReference userDb;
+
+    public DAO()
+    {
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        requestDb = db.getReference(Request.class.getSimpleName());
+        userDb = db.getReference(User.class.getSimpleName());
+    }
+
+    public Task<Void> add(SmolRequest req){
+        return requestDb.child(DB_USER_ID).push().setValue(req);
+    }
+
+    public Task<Void> addRequest() {
+        String key = requestDb.push().getKey();
+        Order order = new Order("senat house", "senat kitchen", "varshini", true);
+        User user = new User("senat", "senat");
+        Request request = new Request(user, order, System.currentTimeMillis(), 0L, "SUTD", false, false);
+        Map<String, Object> requestValues = request.toMap();
+
+        Map<String, Object> childUpdates = new HashMap<>();
+        childUpdates.put(key, requestValues);
+
+        return requestDb.updateChildren(childUpdates);
+    }
+
+}
