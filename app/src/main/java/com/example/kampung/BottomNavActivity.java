0aa2ddaf39243f61f2da1af9b96ca25b88e80375
kellieayy <kellie.awyong@gmail.com>
2022-04-08 17:20:47 +0800

    Basic log in function

--- app/src/main/java/com/example/kampung/BottomNavActivity.java
+++ app/src/main/java/com/example/kampung/BottomNavActivity.java
@@ -32,6 +32,7 @@ public class BottomNavActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_bottom_nav);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
     }
 
 }
