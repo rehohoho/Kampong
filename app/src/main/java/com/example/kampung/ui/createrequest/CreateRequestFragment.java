6e362047a0c917d90b021b6f0196b9512411f9fd
kellieayy <kellie.awyong@gmail.com>
2022-04-09 15:41:57 +0800

    login + createreqfrag skeleton

--- app/src/main/java/com/example/kampung/ui/createrequest/CreateRequestFragment.java
+++ app/src/main/java/com/example/kampung/ui/createrequest/CreateRequestFragment.java
@@ -1,25 +1,22 @@
-package com.example.kampung.ui.dashboard;
+package com.example.kampung.ui.createrequest;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.kampung.R;
-import com.example.kampung.databinding.FragmentDashboardBinding;
 
-public class DashboardFragment extends Fragment {
+public class CreateRequestFragment extends Fragment {
 
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View dashboardView =inflater.inflate(R.layout.fragment_dashboard, container, false);
+        View dashboardView =inflater.inflate(R.layout.fragment_create_req, container, false);
         return dashboardView;
     }
 
