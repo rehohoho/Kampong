3746f144093e926ce2da726abb5e084c4d670373
itsBaka <itsBaka@users.noreply.github.com>
2022-04-17 14:44:10 +0800

    link search to home

--- app/src/main/java/com/example/kampung/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/kampung/ui/home/HomeFragment.java
@@ -9,8 +9,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.kampung.R;
@@ -19,12 +22,8 @@ import com.example.kampung.controllers.RequestsViewModel;
 import com.example.kampung.databinding.FragmentHomeBinding;
 import com.example.kampung.models.RequestAction;
 import com.example.kampung.ui.search.SearchActivity;
-import com.example.kampung.ui.search.SearchRequestAdapter;
 
 import java.util.ArrayList;
-import java.util.Observable;
-import java.util.Observer;
-
 
 public class HomeFragment extends Fragment {
 
@@ -36,37 +35,48 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
+
+        binding.icSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AppCompatActivity activity = (AppCompatActivity) view.getContext();
+                NavController navController = Navigation.findNavController(activity, R.id.nav_host_fragment_activity_bottom_nav);
+                navController.navigate(R.id.action_navigation_home_to_search_fragment);
+            }
+        });
+
         return binding.getRoot();
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        TextView text = ((TextView) view.findViewById(R.id.text_request));
+        String searchString = SearchActivity.query;
 
-        if (SearchActivity.query.length() > 0) {
-            Log.d(TAG, "View Created " + SearchActivity.query + " queries");
-            SearchRequestAdapter requestAdapter = new SearchRequestAdapter(getContext(), requestList);
+        if (searchString.length() > 0) {
+            Log.d(TAG, "View Created " + searchString + " queries");
+            binding.textRequest.setText("Filtered Result");
+            HomeRequestAdapter requestAdapter = new HomeRequestAdapter(getContext(), requestList);
             binding.recyclerBrowsereq.setAdapter(requestAdapter);
             binding.recyclerBrowsereq.setLayoutManager(new LinearLayoutManager(getContext()));
             requestsViewModel = new ViewModelProvider(this).get(RequestsViewModel.class);
             requestsViewModel.getRequests(DAO.getInstance()).observe(getViewLifecycleOwner(), request -> {
-                if (request.getRequest().order.location.equals(SearchActivity.query)) {
+                if (request.getRequest().order.location.equals(searchString) && !request.getRequest().isAccepted && request.getActionId() == RequestAction.ACTION_ID.ADDED) {
                     requestList.add(request);
                     requestAdapter.notifyItemInserted(requestList.size() - 1);
                 }
-                // DISPLAY TOASTS FOR SUCCESSFUL / UNSUCCESSFUL SEARCHES
+                // DISPLAY MESSAGE FOR SUCCESSFUL / UNSUCCESSFUL SEARCHES
                 if (requestList.isEmpty()) {
-                    text.setText("No requests were found.");
+                    binding.recyclerBrowsereq.setVisibility(View.GONE);
+                    binding.emptyView.setText("No request found");
+                    binding.emptyView.setVisibility(View.VISIBLE);
+                } else {
+                    binding.recyclerBrowsereq.setVisibility(View.VISIBLE);
+                    binding.emptyView.setVisibility(View.GONE);
+                }
 
-                    Toast.makeText(getActivity(), "No requests with " + SearchActivity.query + " was found.", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    text.setText("Filter-" + SearchActivity.query);
-                    Toast.makeText(getActivity(), "Displaying " + SearchActivity.query + " queries", Toast.LENGTH_SHORT).show();
-                }
-                // RESET QUERY FROM SEARCHACTIVITY
-                SearchActivity.query = "";
             });
+            // RESET QUERY FROM SEARCHACTIVITY
+            SearchActivity.query = "";
         }
         else {
             Log.d(TAG, "No Search");
@@ -76,10 +86,21 @@ public class HomeFragment extends Fragment {
 
             requestsViewModel = new ViewModelProvider(this).get(RequestsViewModel.class);
             requestsViewModel.getRequests(DAO.getInstance()).observe(getViewLifecycleOwner(), request -> {
-                if(!request.getRequest().isDelivered && request.getActionId() == RequestAction.ACTION_ID.ADDED){
+                if(!request.getRequest().isAccepted && request.getActionId() == RequestAction.ACTION_ID.ADDED){
                     requestList.add(request);
                     requestAdapter.notifyItemInserted(requestList.size() - 1);
                 }
+
+                if (requestList.isEmpty()) {
+                    binding.recyclerBrowsereq.setVisibility(View.GONE);
+                    binding.emptyView.setText("No request available");
+                    binding.emptyView.setVisibility(View.VISIBLE);
+                } else {
+                    binding.recyclerBrowsereq.setVisibility(View.VISIBLE);
+                    binding.emptyView.setVisibility(View.GONE);
+                }
+
+
             });
         }
 
